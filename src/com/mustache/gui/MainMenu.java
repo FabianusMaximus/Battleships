846f6ef77676b1181327ac33b3b20bc0e6a1c6ba
Wipeshot <domikel02@outlook.de>
2022-01-15 13:44:09 +0100

    UI Update

--- src/com/mustache/gui/MainMenu.java
+++ src/com/mustache/gui/MainMenu.java
@@ -1,20 +1,40 @@
 package com.mustache.gui;
 
+import com.mustache.main.Controller;
+import com.mustache.translate.Translate;
+
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class MainMenu {
 
-    private JFrame window;
+    private final Translate translate = new Translate("english", "german");
 
-    private JButton button;
+    private final JPanel contentPane = new JPanel(null);
+    private final JPanel panelButtons = new JPanel(new GridLayout(4,1));
 
-    public MainMenu(JFrame frame) {
-        window = frame;
+    private final JButton singleplayer = new JButton(translate.translated("Singleplayer"));
+    private final JButton multiplayer = new JButton(translate.translated("Multiplayer"));
+    private final JButton options = new JButton(translate.translated("Options"));
+    private final JButton exit = new JButton(translate.translated("Exit"));
+
+    private final JButton changeLanguage = new JButton("German");
+
+    public MainMenu(Controller con) throws IOException {
         setupMainMenu();
+        con.setWindow(contentPane);
     }
 
     private void setupMainMenu() {
-
+        panelButtons.add(singleplayer);
+        panelButtons.add(multiplayer);
+        panelButtons.add(options);
+        panelButtons.add(exit);
+        panelButtons.setBounds(400, 200, 200, 100);
+        changeLanguage.setBounds(970, 490, 30, 10);
+        contentPane.add(panelButtons);
+        contentPane.add(changeLanguage);
     }
 
 }
--- src/com/mustache/main/Controller.java
+++ src/com/mustache/main/Controller.java
@@ -3,18 +3,31 @@ package com.mustache.main;
 import com.mustache.gui.MainMenu;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class Controller {
 
-    private JFrame window = new JFrame();
+    private final JFrame window = new JFrame();
+    private JPanel contentPane;
 
-    public Controller() {
-        new MainMenu(window);
+    public Controller() throws IOException {
+        new MainMenu(this);
+        setupWindow();
+        window.repaint();
+    }
+
+
+    public void setWindow(JPanel contentPane) {
+        this.contentPane = contentPane;
     }
 
     private void setupWindow() {
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setSize(1000,500);
         window.setResizable(false);
+        window.setVisible(true);
+        window.add(contentPane);
     }
 
 
@@ -22,7 +35,7 @@ public class Controller {
 
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Controller();
     }
 }
